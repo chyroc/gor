6f96dd35e081ce4ec57b83faee381636f9683a7b
Chyroc <chen_yunpeng@foxmail.com>
2017-11-24 00:16:08 +0800

    fix golint

--- .circleci/check_code_style.sh
+++ .circleci/check_code_style.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 
-set -e
-
 FMT_STATUS=`gofmt -d $(find . -type f -name '*.go' -not -path "./vendor/*" -not -path "./_project/*")`
 if ["$FMT_STATUS" == ""]; then exit 0; else exit 1; fi
-
-sdaasdf asf
-ls
-echo $(go list ./... | grep -v /vendor/)
-
-LINT_STATUS=`golint $(go list ./... | grep -v /vendor/)`
-if ["$LINT_STATUS" == ""]; then exit 0; else exit 1; fi
