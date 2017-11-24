96dfe9bc1a4b7b6f3a358c8f0e15c836a9fd9855
Chyroc <chen_yunpeng@foxmail.com>
2017-11-24 16:24:35 +0800

    Add/redirect end status (#8)
    * add redirect
    
    * add end
    
    * add status

--- res.go
+++ res.go
@@ -22,6 +22,21 @@ func (res *Res) errResponseTypeUnsupported(vtype string, v interface{}) {
 	http.Error(res, fmt.Sprintf("[%s] [%s] %+v", ErrResponseTypeUnsupported, vtype, v), http.StatusInternalServerError)
 }
 
+// Status set response http status code
+func (res *Res) Status(code int) *Res {
+	if http.StatusText(code) == "" {
+		http.Error(res, ErrHTTPStatusCodeInvalid.Error(), http.StatusInternalServerError)
+		return res
+	}
+	res.WriteHeader(code)
+	return res
+}
+
+// SendStatus set response http status code with its text
+func (res *Res) SendStatus(code int) {
+	res.Status(code).Send(http.StatusText(code))
+}
+
 // Send Send a response
 func (res *Res) Send(v string) {
 	res.Write([]byte(v))
@@ -52,4 +67,15 @@ func (res *Res) JSON(v interface{}) {
 
 	res.Header().Set("Content-Type", "application/json")
 	res.Write(b)
+}
+
+// Redirect Redirect to another url
+func (res *Res) Redirect(path string) {
+	res.Header().Set("Location", path)
+	res.WriteHeader(http.StatusFound)
+	res.Write([]byte(fmt.Sprintf(`%s. Redirecting to %s`, http.StatusText(http.StatusFound), path)))
+}
+
+// End end the request
+func (res *Res) End() {
 }
