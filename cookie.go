780b19f31ed630a072af6fb87652f4deb013993f
Chyroc <chen_yunpeng@foxmail.com>
2017-11-24 19:10:00 +0800

    add add-heaser-and-set-cookie (#9)

---
+++ cookie.go
@@ -0,0 +1,41 @@
+package gor
+
+import (
+	"net/http"
+	"time"
+)
+
+// Cookie like http.Cookie but remove name and value
+type Cookie struct {
+	Path       string    // optional
+	Domain     string    // optional
+	Expires    time.Time // optional
+	RawExpires string    // for reading cookies only
+
+	// MaxAge=0 means no 'Max-Age' attribute specified.
+	// MaxAge<0 means delete cookie now, equivalently 'Max-Age: 0'
+	// MaxAge>0 means Max-Age attribute present and given in seconds
+	MaxAge   int
+	Secure   bool
+	HTTPOnly bool
+	Raw      string
+	Unparsed []string // Raw text of unparsed attribute-value pairs
+}
+
+func (c *Cookie) toHTTPCookie(key, val string) *http.Cookie {
+	return &http.Cookie{
+		Name:  key,
+		Value: val,
+
+		Path:       c.Path,
+		Domain:     c.Domain,
+		Expires:    c.Expires,
+		RawExpires: c.RawExpires,
+
+		MaxAge:   c.MaxAge,
+		Secure:   c.Secure,
+		HttpOnly: c.HTTPOnly,
+		Raw:      c.Raw,
+		Unparsed: c.Unparsed,
+	}
+}
--- res.go
+++ res.go
@@ -10,11 +10,13 @@ import (
 // Res is http ResponseWriter and some gor Response method
 type Res struct {
 	http.ResponseWriter
+	exit bool
 }
 
 func httpResponseWriterToRes(httpResponseWriter http.ResponseWriter) Res {
 	return Res{
 		httpResponseWriter,
+		false,
 	}
 }
 
@@ -26,9 +28,11 @@ func (res *Res) errResponseTypeUnsupported(vtype string, v interface{}) {
 func (res *Res) Status(code int) *Res {
 	if http.StatusText(code) == "" {
 		http.Error(res, ErrHTTPStatusCodeInvalid.Error(), http.StatusInternalServerError)
-		return res
+		res.exit = true
+	} else {
+		res.WriteHeader(code)
 	}
-	res.WriteHeader(code)
+
 	return res
 }
 
@@ -39,11 +43,24 @@ func (res *Res) SendStatus(code int) {
 
 // Send Send a response
 func (res *Res) Send(v string) {
+	if res.exit {
+		return
+	}
+
 	res.Write([]byte(v))
+	res.exit = true
 }
 
 // JSON Send json response
 func (res *Res) JSON(v interface{}) {
+	defer func() {
+		res.exit = true
+	}()
+
+	if res.exit {
+		return
+	}
+
 	if v == nil {
 		res.errResponseTypeUnsupported("nil", v)
 		return
@@ -76,6 +93,35 @@ func (res *Res) Redirect(path string) {
 	res.Write([]byte(fmt.Sprintf(`%s. Redirecting to %s`, http.StatusText(http.StatusFound), path)))
 }
 
+// AddHeader append (key, val) to headers
+func (res *Res) AddHeader(key, val string) {
+	res.Header().Add(key, val)
+}
+
+// SetCookie set cookie
+func (res *Res) SetCookie(key, val string, option ...Cookie) {
+	var cookie *http.Cookie
+	if len(option) > 1 {
+		res.Error("only support one cookie option")
+	} else if len(option) == 1 {
+		cookie = option[0].toHTTPCookie(key, val)
+	} else {
+		cookie = &http.Cookie{
+			Name:  key,
+			Value: val,
+		}
+	}
+
+	http.SetCookie(res, cookie)
+}
+
+// Error send erroe response
+func (res *Res) Error(v string) {
+	http.Error(res, v, http.StatusInternalServerError)
+	res.exit = true
+}
+
 // End end the request
 func (res *Res) End() {
+	res.exit = true
 }
