b57b18a33ca16cbceeb2e3d0b3de53d978bb0c40
Chyroc <chen_yunpeng@foxmail.com>
2017-11-25 10:12:15 +0800

    add req-query

--- req.go
+++ req.go
@@ -3,19 +3,43 @@ package gor
 import (
 	"context"
 	"net/http"
+	"net/url"
 )
 
 // Req is http Request struct
 type Req struct {
-	httpr   *http.Request
+	r       *http.Request
 	context context.Context
+
+	Method string
+	Query  map[string][]string
 }
 
-func httpRequestToReq(httpRequest *http.Request) *Req {
-	return &Req{
-		httpRequest,
-		httpRequest.Context(),
+func getQuery(r *http.Request) (map[string][]string, error) {
+	URL, err := url.Parse(r.URL.Path)
+	if err != nil {
+		return nil, err
 	}
+	query, err := url.ParseQuery(URL.RawQuery)
+	if err != nil {
+		return nil, err
+	}
+	return query, nil
+}
+
+func httpRequestToReq(r *http.Request) (*Req, error) {
+	query, err := getQuery(r)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Req{
+		r:       r,
+		context: r.Context(),
+
+		Method: r.Method,
+		Query:  query,
+	}, nil
 }
 
 // AddContext add value to gor context
